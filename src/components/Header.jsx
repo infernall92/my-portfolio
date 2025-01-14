83fa37e1b6287a16fd2e0c3ba5efe76ccb97b5a2
Hristo <hhristo.hristovv@gmail.com>
2025-01-14 11:11:13 +0200

    tailwind install

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return <header className="flex">
+        <h1 className="text-red-500">MY PORTFOLIO</h1>
+        <h2>this is H2</h2>
+        <p>this is paragraph</p>
+    </header>
+}
