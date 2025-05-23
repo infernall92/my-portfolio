9c8de5c1dcfa3c0f30ab72620cec4f46f73cb919
Hristo <hhristo.hristovv@gmail.com>
2025-05-23 15:13:36 +0300

    modify experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,21 +1,26 @@
 import Card from "./Card";
 import EXPERIENCE from "../experience.json";
+import ScrollFadeInWrapper from "./custom/ScrollFadeInWrapper";
 const Experience = () => {
   return (
     <section id="experience" className="mt-16">
+      <h2 className="md:hidden text-6xl font-thin text-slate-300 mb-10">
+        <span className="font-bold">EXP</span>ERIENCE
+      </h2>
       {EXPERIENCE.map((exp, index) => (
-        <Card
-          key={index}
-          from={exp.time.from}
-          to={exp.time.to}
-          role={exp.role}
-          place={exp.workplace}
-          description={exp.description}
-          technologies={exp.technologies}
-          working
-          linkLabel={"KONGWallet Page"}
-          href={exp.link}
-        />
+        <ScrollFadeInWrapper key={index}>
+          <Card
+            from={exp.time.from}
+            to={exp.time.to}
+            role={exp.role}
+            place={exp.workplace}
+            description={exp.description}
+            technologies={exp.technologies}
+            working
+            linkLabel={"KONGWallet Page"}
+            href={exp.link}
+          />
+        </ScrollFadeInWrapper>
       ))}
     </section>
   );
